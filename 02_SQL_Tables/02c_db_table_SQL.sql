2a7f3884d42740966fe1a4bcbc676cb2e8861b0e
YasmeenMas <yasmeenmas@gmx.de>
2023-08-28 09:35:27 +0000

    UNIQUE

---
+++ 02_SQL_Tables/02c_db_table_SQL.sql
@@ -0,0 +1,60 @@
+/* ------  Strukturen ----- */
+
+/* Kommentar 1 */
+#  Kommentar 2
+-- Kommentar 3
+
+/* Datenbanken auf Server anzeigen */
+SHOW DATABASES;
+
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+/* DB auswählen */
+USE boo;
+
+/* ------  Strukturen ----- */
+/*      
+    NULL wird nicht mehr zugelassen
+    DEFAULT-Werte eintragen
+*/ 
+
+/*      
+
+    Doppelte DS werden nicht mehr zugelassen
+
+    UNIQUE
+
+*/
+
+/* Tabelle cats anlegen, falls noch nicht vorhanden*/
+CREATE TABLE IF NOT EXISTS cats
+(
+    name VARCHAR(20) NOT NULL UNIQUE DEFAULT "TBA",
+    age INT NOT NULL DEFAULT 0
+);
+ 
+/* Alle Tabellen in der DB anzeigen */ 
+# SHOW TABLES;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE cats;
+
+
+/* ----- Daten ------- */
+INSERT INTO cats(name,age) VALUES ("Grizablla",29);
+INSERT INTO cats(age,name) VALUES (35,"Alonzo");
+INSERT INTO cats VALUES ();
+
+-- ABER: Doppelte Datensätze werden zugelassen !
+INSERT INTO cats(age,name) VALUES (35,"Alonzo, der Coole!");
+
+
+
+/* ---- Inhalte der Tabelle anzeigen ---- */
+SELECT * FROM cats;
