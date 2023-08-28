23103c21d641a40f952170c85d6648f8ac671258
YasmeenMas <yasmeenmas@gmx.de>
2023-08-28 07:11:40 +0000

    DB  löschen,  anlegen

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -7,3 +7,16 @@
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
 
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+SHOW DATABASES;
+
+/* DB auswählen */
+USE boo;
+ 
+/* Alle Tabellen in der DB anzeigen */ 
+SHOW TABLES;
