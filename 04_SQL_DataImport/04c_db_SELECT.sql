3a315243bf89277ff3473f3d62f943c57bbb53a4
YasmeenMas <yasmeenmas@gmx.de>
2023-08-30 08:17:17 +0000

    Data import

--- 04_SQL_DataImport/04c_db_SELECT.sql
+++ 04_SQL_DataImport/04c_db_SELECT.sql
@@ -6,18 +6,13 @@ SELECT * FROM boo.cats;
 
 /* UPDATE --> Änderung(en) in bestehender Tab. */
 UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
-
 UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
 
 /* READ - SELECT : NEUE Ergebnistabelle wird erstellt */
 
 # SELECT * FROM boo.cats; -- gesamte Tabelle
-
 # SELECT cat_name FROM boo.cats; -- einzelnes Feld
-
-
 # SELECT age,cat_name FROM boo.cats; -- Felder kombiniert
-
 # SELECT cat_name AS "Katzen" FROM boo.cats; -- ALias
 
 /*Filtern durch WHERE*/
@@ -33,9 +28,12 @@ WHERE fur_color = "grey"
 */
 
 /* Delete - DS löschen | Vorsicht!! */
+/* Bitte NIE mit root-Rechten!!! */
 
 DELETE FROM boo.cats WHERE id = 2; -- Filter
-
 #DELETE FROM boo.cats -- Alle Daten sind weg!!
 
-/* Bitte NIE mit root-Rechten!!! */
+
+DESCRIBE boo.cats;
+
+SELECT * FROM boo.cats;
