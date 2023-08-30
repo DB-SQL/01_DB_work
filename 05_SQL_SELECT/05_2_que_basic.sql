f0978f7b6f86d2f3141224ac238d72c9701c06a8
YasmeenMas <yasmeenmas@gmx.de>
2023-08-30 10:07:44 +0000

    QUERIES Basic

---
+++ 05_SQL_SELECT/05_2_que_basic.sql
@@ -0,0 +1,26 @@
+/* QUERIES Basic */
+
+/*
+Felder abfragen
+Felder kombinieren
+Ausgabe beschränken
+*/
+
+-- Abfrage aller Spalten der Tab
+#SELECT * FROM stocks.ccc;
+
+-- Begrenzung: LIMIT
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+-- Kombinationen aus mehreren Feldern in neues Feld 
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
+SELECT
+    ticker AS "SYM",
+    c_name AS Unternehmen,
+    # industry Branche
+    # sector Industriesektor
+    CONCAT(sector,"|",industry) AS "Operations"
+FROM stocks.ccc
+LIMIT 10
+# LIMIT 1
+# LIMIT 200,10 -- offset
+;
