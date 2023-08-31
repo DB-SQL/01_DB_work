ad491d13c593c1603a57a2f651179be85cf76446
YasmeenMas <yasmeenmas@gmx.de>
2023-08-31 10:34:55 +0000

    ...

--- 05_SQL_SELECT/05_6d_que_avg.sql
+++ 05_SQL_SELECT/05_6d_que_avg.sql
@@ -3,9 +3,9 @@
 -- Durchschnittspreis einer Aktie / Banken?
 /**/
 SELECT
-    round(avg(price),2) Durchschnittspreis
+    ROUND(AVG(price),2) Durchschnittspreis
 FROM stocks.ccc
-#WHERE industry = "Banks"
+# WHERE industry = "Banks"
 WHERE industry = "Media"
 ;
 
@@ -14,8 +14,8 @@ WHERE industry = "Media"
 -- KOMBINATION Preis/Dividende pro Industriesektor
 SELECT
     sector Industriesektor, -- organisch
-    round(avg(price),2) Durchschnittspreis, -- aggregiert (berechnet)
-    avg(dividend) Durchschnittsdividende -- aggregiert (berechnet)
+    ROUND(AVG(price),2) Durchschnittspreis, -- aggregiert (berechnet)
+    AVG(dividend) Durchschnittsdividende -- aggregiert (berechnet)
 FROM stocks.ccc
 GROUP BY sector -- Gruppieren nach organ. Daten/Spalte/Feld
 #ORDER BY Durchschnittspreis DESC
